4b4ee8d25ea1633b4ea8c56c96e4c2ac61d3ca20
Kumar Aditya <kaditya@smartdatainc.net>
2015-05-06 16:26:36 +0530

    Added data flow for match screen

--- AncestorCloud/Development/Application/SourceCode/AncestorCloud/AncestorCloud.Touch/Views/MatcherDetail/RelationshipMatchDetailView.cs
+++ AncestorCloud/Development/Application/SourceCode/AncestorCloud/AncestorCloud.Touch/Views/MatcherDetail/RelationshipMatchDetailView.cs
@@ -9,14 +9,12 @@ using Cirrious.MvvmCross.Plugins.Messenger;
 using AncestorCloud.Shared;
 using AncestorCloud.Shared.ViewModels;
 using System.Drawing;
+using Cirrious.MvvmCross.Binding.BindingContext;
 
 namespace AncestorCloud.Touch
 {
 	public partial class RelationshipMatchDetailView : BaseViewController
 	{
-
-		UITableView table;
-
 		public RelationshipMatchDetailView () : base ("RelationshipMatchDetailView", null)
 		{
 		}
@@ -39,23 +37,31 @@ namespace AncestorCloud.Touch
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
-			CreateRelationShipTable();
+			SetRelationShipDetailView();
+			BindData ();
 			
 			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void ViewWillDisappear (bool animated)
+		{
+
+			if (!NavigationController.ViewControllers.Contains (this)) {
+				var messenger = Mvx.Resolve<IMvxMessenger> ();
+				messenger.Publish (new NavigationBarHiddenMessage (this, true)); 
+
+			}
+			base.ViewWillDisappear (animated);
 		}
 
 
 		#region Relationship table
 
-		private void CreateRelationShipTable()
+		private void SetRelationShipDetailView()
 		{
-
 			this.Title = "Matcher";
 			this.NavigationController.NavigationBar.TintColor=UIColor.FromRGB(255,255,255);
 
-			table = RelationshipMatchTable;//new UITableView(View.Bounds); // defaults to Plain style
-			string[] tableItems = new string[] {"1972"+"   "+"Glenneth Girtrude Gates"+"  " +"    06º","1956"+"   "+"Henry Wright Gates"+"          " +"    07º","1925"+"   "+"Glenneth Girtrude Gates"+"  " +"    08º","1907"+"   "+"Henry Wright Gates"+"          " +"    09º","1925"+"   "+"Henry Wright Gates"+"          " +"    10º","1907"+"   "+"Glenneth Girtrude Gates"+"  " +"    11º","1925"+"   "+"Henry Wright Gates"+"          " +"    12º","1921"+"   "+"Glenneth Girtrude Gates"+"  " +"    13º","1972"+"   "+"Henry Wright Gates"+"          " +"    14º","1956"+"   "+"Glenneth Girtrude Gates"+"  " +"    15º"};
-			table.Source = new RelationshipMatchTableSource(tableItems);
 			this.NavigationItem.TitleView = new MyMatchTitleView (this.Title,new RectangleF(0,0,150,20));
 			this.NavigationController.NavigationBarHidden = false;
 
@@ -76,16 +82,22 @@ namespace AncestorCloud.Touch
 
 		#endregion
 
-		public override void ViewWillDisappear (bool animated)
+		void BindData()
 		{
+			var source = new RelationshipMatchTableSource (RelationshipMatchTable);
+			//var source = new MvxSimpleTableViewSource(fbFamilyTableView, FbFamilyCell.Key, FbFamilyCell.Key);
+			RelationshipMatchTable.Source = source;
 
-			if (!NavigationController.ViewControllers.Contains (this)) {
-				var messenger = Mvx.Resolve<IMvxMessenger> ();
-				messenger.Publish (new NavigationBarHiddenMessage (this, true)); 
-			
-			}
-			base.ViewWillDisappear (animated);
+			var set = this.CreateBindingSet<RelationshipMatchDetailView , RelationshipMatchDetailViewModel > ();
+			set.Bind (source).To (vm => vm.MatchResultList);
+			set.Apply ();
 		}
+
+		#region Data Binding
+
+		#endregion
+
+
 
 	}
 }
