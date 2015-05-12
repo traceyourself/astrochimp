902d7ef3fc6e3750c2d2d989328aae9a50a4297b
Kumar Aditya <kaditya@smartdatainc.net>
2015-05-12 16:09:07 +0530

    Added friend list in Matcher.

--- AncestorCloud/Development/Application/SourceCode/AncestorCloud/AncestorCloud.Shared/ViewModels/LoginViewModel.cs
+++ AncestorCloud/Development/Application/SourceCode/AncestorCloud/AncestorCloud.Shared/ViewModels/LoginViewModel.cs
@@ -324,7 +324,7 @@ namespace AncestorCloud.Shared.ViewModels
 			ResponseStatus status = await _facebookLinkManager.LinkFaceBookLoginUser ();
 
 			if (status == ResponseStatus.Fail) {
-				Alert.ShowAlert ("Not able to link Faccebbok user to OGF. Please retry by log-in again", "ERROR");
+				Alert.ShowAlert ("Not able to link Facebook user to OGF. Please retry by log-in again", "ERROR");
 			} else {
 				Login ();
 			}
