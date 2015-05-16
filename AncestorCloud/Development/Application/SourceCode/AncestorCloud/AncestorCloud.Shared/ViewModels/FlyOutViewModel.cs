2179b3da1bdd07b8a47c5043459897d5d1183751
Kumar Aditya <kaditya@smartdatainc.net>
2015-05-16 18:34:17 +0530

    add fixes

--- AncestorCloud/Development/Application/SourceCode/AncestorCloud/AncestorCloud.Shared/ViewModels/FlyOutViewModel.cs
+++ AncestorCloud/Development/Application/SourceCode/AncestorCloud/AncestorCloud.Shared/ViewModels/FlyOutViewModel.cs
@@ -23,11 +23,6 @@ namespace AncestorCloud.Shared.ViewModels
 			this.SetItemList (IsFaceBookLogin);
 		}
 
-		public FlyOutViewModel(IDatabaseService  service)
-		{
-			_databaseService = service;
-		}
-
 
 		#region get Userdata method
 		public LoginModel GetUserData()
@@ -44,11 +39,13 @@ namespace AncestorCloud.Shared.ViewModels
 		}
 		#endregion
 
-		public FlyOutViewModel ()
+		public FlyOutViewModel (IDatabaseService service)
 		{
 
 //			var _messenger = Mvx.Resolve<IMvxMessenger>();
 //			navigationMenuToggleToken = _messenger.SubscribeOnMainThread<Message>(message => this.Close(this));
+
+			_databaseService = service;
 
 			var _flyoutMessenger = Mvx.Resolve<IMvxMessenger>();
 			changeFlyoutToken = _flyoutMessenger.SubscribeOnMainThread<ChangeFlyoutFlowMessage>(message => this.ReloadMenuList(message.ChangeFlyoutFlow));
