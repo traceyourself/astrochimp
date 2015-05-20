eeb814638711e164ba9d1774778fcbac4865356f
Kumar Aditya <kaditya@smartdatainc.net>
2015-05-20 16:39:11 +0530

    Fixed file path issue

--- AncestorCloud/Development/Application/SourceCode/AncestorCloud/AncestorCloud.Touch/Views/ProfilePic/ProfilePicView.cs
+++ AncestorCloud/Development/Application/SourceCode/AncestorCloud/AncestorCloud.Touch/Views/ProfilePic/ProfilePicView.cs
@@ -286,13 +286,10 @@ namespace AncestorCloud.Touch
 				AppDelegate appDelegate = (AppDelegate)UIApplication.SharedApplication.Delegate;
 				appDelegate.UIImageProfilePic = originalImage;
 
-//				var documentsDirectory = Environment.GetFolderPath
-//					(Environment.SpecialFolder.MyDocuments);
+				var documentsDirectory = Environment.GetFolderPath
+					(Environment.SpecialFolder.MyDocuments);
 
-				var documents = NSFileManager.DefaultManager.GetUrls (NSSearchPathDirectory.CachesDirectory, NSSearchPathDomain.User) [0];
-				string documentsDirectory = documents.ToString();
-
-				string jpgFilename = System.IO.Path.Combine (documentsDirectory, "ProfilePic.jpg");
+				string jpgFilename = System.IO.Path.Combine (documentsDirectory, "../Library/Caches/ProfilePic.jpg");
 				NSData imgData = originalImage.AsJPEG();
 				NSError err = null;
 				if (imgData.Save(jpgFilename, false, out err))
